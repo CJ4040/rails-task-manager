775e06e68c8d262a2667d70b915f85507162f228
Chris Cashin <cashin.cj@gmail.com>
2024-09-20 16:00:34 +0200

    added create function to tasks controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   get "/tasks", to: "tasks#index"
+  get "/tasks/new", to: "tasks#new"
+  post "/tasks", to: "tasks#create"
   get "/tasks/:id", to: "tasks#show", as: "task"
 
 end
